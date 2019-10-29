1eb5ef648107242533ace8e23ba1b5489d6fd1ef
Luukvdm <luuk.vdm@hotmail.com>
2019-10-29 20:36:49 +0100

    Small fixes and updates

--- bash/.bashrc.d/aliases.sh
+++ bash/.bashrc.d/aliases.sh
@@ -1,8 +1,16 @@
 
+alias sudo='sudo '
 alias ls='ls --color=auto -w 70'
-alias vim='nvim'
 alias ..='cd ..'
 alias df='df -h'
 alias du='du -ch'
 alias ipp='curl ipinfo.io/ip'
+
+if hash nvim 2>/dev/null; then
+	alias vim='nvim'
+fi
+
+if hash neofetch 2>/dev/null; then
+	alias neofetch='neofetch --ascii ~/.config/neofetch/ascii.txt'
+fi
 
---
+++ misc/scripts/onedrive-setup.sh
@@ -0,0 +1,7 @@
+mkdir ~/onedrive-$1
+
+mkdir ~/.config/onedrive
+mkdir ~/.config/onedrive/$1
+
+#chmod a+x ~/.config/systemd/user/onedrive-$1.service
+
