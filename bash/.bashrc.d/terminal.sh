95f6d0490d67c0bd980c788f679944d9767337b6
Luuk van den Maagdenberg <luuk.vdm@hotmail.com>
2019-11-27 22:04:33 +0100

    Added color to hostname

--- bash/.bashrc.d/terminal.sh
+++ bash/.bashrc.d/terminal.sh
@@ -1,10 +1,11 @@
 color='\033[38;5;33m\]'
+hostnameColor='\033[38;5;214m\]'
 if [ $(id -u) -eq 0 ]; then # root
 	color='\033[38;5;196m\]'
 fi
-
+# \[\033[38;5;15m\] 
 if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
-	PS1=" \h \[$(tput bold)\]\[$(tput sgr0)\]\[$color\w\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"
+	PS1=" \[$(tput sgr0)\]\[$hostnameColor\h\[$(tput sgr0)\] \[$(tput bold)\]\[$(tput sgr0)\]\[$color\w\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"
 else
 	PS1=" \[$(tput bold)\]\[$(tput sgr0)\]\[$color\w\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"
 fi
