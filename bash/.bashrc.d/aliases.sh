a46943c5344df3068243022b0176902977e8444d
Luuk van den Maagdenberg <luuk.vdm@hotmail.com>
2020-03-30 19:08:42 +0000

    Added some aliasses

--- bash/.bashrc.d/aliases.sh
+++ bash/.bashrc.d/aliases.sh
@@ -6,6 +6,18 @@ alias df='df -h'
 alias du='du -ch'
 alias ipp='curl ipinfo.io/ip'
 alias ip='ip -c'
+alias gitlog='git log --stat --pretty=short --graph'
+alias serve='echo Live on: http://localhost:8000 && python -m http.server && open http://localhost:8000'
+alias aliases='vim ~/dotfiles/bash/.bashrc.d/aliases.sh'
+
+# Systemd dingen
+alias service='sudo systemctl'
+alias userctl='systemctl --user'
+alias restart='service restart'
+alias status='systemctl status'
+
+# Logging dingen
+alias errors='journalctl -p err..alert --since "24 h ago"'
 
 #if type pacman &> /dev/null; then
     # alias pacman -Syu ='sudo pacman -Syu'
