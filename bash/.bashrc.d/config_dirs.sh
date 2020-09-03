18422aac88de10272a88f9728b26db7f195d48d7
Luuk van den Maagdenberg <luuk.vdm@hotmail.com>
2020-09-03 18:10:11 +0200

    Changed configs

---
+++ bash/.bashrc.d/config_dirs.sh
@@ -0,0 +1,43 @@
+export BASH_COMPLETION_USER_FILE="$XDG_CONFIG_HOME"/bash-completion/bash_completion
+
+export IDEA_PROPERTIES="$XDG_CONFIG_HOME"/intellij-idea/idea.properties
+export IDEA_VM_OPTIONS="$XDG_CONFIG_HOME"/intellij-idea/idea.vmoptions
+export STUDIO_PROPERTIES="$XDG_CONFIG_HOME"/android-studio/idea.properties
+export GOLAND_PROPERTIES="$XDG_CONFIG_HOME"/goland/idea.properties
+export RIDER_PROPERTIES="$XDG_CONFIG_HOME"/rider/idea.properties
+
+export VSCODE_PORTABLE="$XDG_DATA_HOME"/vscode
+
+export ANDROID_SDK_HOME="$XDG_CONFIG_HOME"/android
+export ANDROID_AVD_HOME="$XDG_DATA_HOME"/android/
+export ANDROID_EMULATOR_HOME="$XDG_DATA_HOME"/android/
+export ADB_VENDOR_KEY="$XDG_CONFIG_HOME"/android
+
+export GNUPGHOME="$XDG_DATA_HOME"/gnupg
+export GTK2_RC_FILES="$XDG_CONFIG_HOME"/gtk-2.0/gtkrc
+export GTK_RC_FILES="$XDG_CONFIG_HOME"/gtk-1.0/gtkrc
+export WGETRC="$XDG_CONFIG_HOME/wgetrc"
+
+export NUGET_PACKAGES="$XDG_CACHE_HOME"/NuGetPackages
+export DOTNET_CLI_HOME="$XDG_CONFIG_HOME"/dotnet
+
+export GRADLE_USER_HOME="$XDG_DATA_HOME"/gradle
+export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
+export CARGO_HOME="$XDG_DATA_HOME"/cargo
+
+export BUNDLE_USER_CONFIG="$XDG_CONFIG_HOME"/bundle
+export BUNDLE_USER_CACHE="$XDG_CACHE_HOME"/bundle
+export BUNDLE_USER_PLUGIN="$XDG_DATA_HOME"/bundle
+
+export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc
+export NPM_CONFIG_CACHE=$XDG_CACHE_HOME/npm
+export NPM_CONFIG_TMP=$XDG_RUNTIME_DIR/npm
+export NODE_REPL_HISTORY="$XDG_DATA_HOME"/node_repl_history
+
+export DOCKER_CONFIG="$XDG_CONFIG_HOME"/docker
+export MACHINE_STORAGE_PATH="$XDG_DATA_HOME"/docker-machine
+
+export MYSQL_HISTFILE="$XDG_DATA_HOME"/mysql_history
+
+alias wget='wget --hsts-file="$XDG_CACHE_HOME/wget-hsts"'
+alias mvn='mvn -gs "$XDG_CONFIG_HOME"/maven/settings.xml'
