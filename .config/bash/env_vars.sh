2e50d306ca3bf62986e86cf895aba598c6e351fb
Luukvdm <luuk.vdm@hotmail.com>
2020-12-03 15:10:05 +0100

    Update env_vars.sh

--- .config/bash/env_vars.sh
+++ .config/bash/env_vars.sh
@@ -1,5 +1,10 @@
-export DOTNET_CLI_TELEMETRY_OPTOUT=1
+if type dotnet &> /dev/null; then
+    export DOTNET_CLI_TELEMETRY_OPTOUT=1
+    export PATH="$PATH:$HOME/.config/dotnet/.dotnet/tools"
+fi
 
 # Only on wayland!
-export MOZ_ENABLE_WAYLAND=1
-export QT_QPA_PLATFORM="xcb"
+if [ -n "$WAYLAND_DISPLAY" ]; then
+    export MOZ_ENABLE_WAYLAND=1
+    export QT_QPA_PLATFORM="xcb"
+fi
